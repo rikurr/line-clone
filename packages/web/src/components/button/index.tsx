1723b96f7ee3aa571c999b272ca2d75a5b010e0b
oya riku <riku03290@gmail.com>
2021-01-03 11:51:06 +0900

    Add:ボタンコンポーネントの作成

---
+++ packages/web/src/components/button/index.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode, ButtonHTMLAttributes } from 'react'
+
+type Props = {
+  children: ReactNode
+  onClick: () => void
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button: React.FC<Props> = ({
+  children,
+  type = 'button',
+  onClick,
+}) => {
+  return (
+    <button
+      type={type}
+      className="bg-primary hover:opacity-80 text-textPrimary font-bold py-2 px-4 rounded"
+      onClick={() => onClick()}
+    >
+      {children}
+    </button>
+  )
+}
--- packages/web/src/pages/home/index.tsx
+++ packages/web/src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import { Header } from '../../components/header'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useGetUserQuery } from '../../types/graphql'
 import { LoadingSpinner } from '../../components/loading-icon'
+import { Button } from '../../components/button'
 
 export const Home: React.FC = () => {
   const { logout, user } = useAuth0()
@@ -23,10 +24,12 @@ export const Home: React.FC = () => {
   return (
     <>
       <Header pageTitle="Home" />
-      <main className="container px-4">
+      <main className="w-full p-4 mx-auto">
         <h1>ログイン完了</h1>
-
-        <button onClick={() => logout({ returnTo: window.location.origin })}>
+        <button
+          className="bg-primary hover:opacity-80 text-textPrimary font-bold py-2 px-4 rounded"
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
           ログアウト
         </button>
       </main>
